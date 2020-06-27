0980bfa715410ccef50222f3def115e597a41dff
Ihor Ostapchuk <ihor.ostapchuk@datarobot.com>
2020-06-27 17:07:21 +0300

    Initial changes

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const auth = require('./modules/auth');
+const homeworkHelp = require('./modules/homework-help');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const fileUpload = require('express-fileupload');
 
 const server = express();
 const port = 3000;
@@ -16,11 +18,17 @@ server.use(
     })
 );
 
+server.use(fileUpload({
+    useTempFiles : true,
+    tempFileDir : '/tmp/',
+    limits: { fileSize: 50 * 1024 * 1024 },
+}));
+
 server.use(
     session({
         secret: 'my super duper secret',
         resave: false,
-        saveUninitialized: false,
+        saveUninitialized: true,
         cookie: {
             maxAge: 60000,
         },
@@ -28,9 +36,14 @@ server.use(
 );
 
 server.use('/auth', auth);
+server.use('/homework-help', homeworkHelp);
 
 server.get('/', (req, res) => {
     res.render('index');
+});
+
+server.get('/asdasd.js', (req, res) => {
+    res.send('HAHA!');
 });
 
 server.listen(port, () => {
