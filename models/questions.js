559d434872a882dd9a0b1fe39f9f17a0b9c39bc2
Ihor Ostapchuk <ihor.ostapchuk@datarobot.com>
2020-07-31 21:58:40 +0300

    Answers functionality beta 0.0.0.0.0.1.0.1.01 :tada:

--- models/questions.js
+++ models/questions.js
@@ -2,7 +2,13 @@ const db = require('../db/mysql');
 
 function fetchQuestions() {
     return new Promise((resolve, reject) => {
-        db.query('SELECT id, title,SUBSTRING(description, 1, 50) AS description, category_name FROM questions LEFT JOIN category ON questions.category_id = category.category_id', (err, results) => {
+        db.query('SELECT questions.id, title,SUBSTRING(description, 1, 50) AS description, category_name, COUNT(answers.id) AS answers_count ' +
+            ' FROM questions ' +
+            ' LEFT JOIN category ' +
+            ' ON questions.category_id = category.category_id ' +
+            ' LEFT JOIN answers ' +
+            ' ON answers.question_id = questions.id ' +
+            ' GROUP BY questions.id', (err, results) => {
             if (err) {
                 reject(err);
             }
@@ -25,4 +31,4 @@ function fetchQuestion(id) {
 module.exports = {
     fetchQuestions,
     fetchQuestion,
-}
+};
